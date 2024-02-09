e3d0f430b86647ba483735189630c5adb6b48829
Lenin Montilla <leninmontilla33@gmail.com>
2024-02-09 09:47:56 -0500

    se creo la api de servicios

--- pbackmande/pbackmande/urls.py
+++ pbackmande/pbackmande/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from servicios.models import *
+from servicios.views import *
+
+router = routers.DefaultRouter()
+router.register(r'servicios', servicios_viewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
