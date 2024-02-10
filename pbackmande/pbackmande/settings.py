825377201b5ef88434bcd32c89f8c0a459ac397c
ccristianss <cristian.silva9402@gmail.com>
2024-02-10 11:57:27 -0500

    Main Funcionando con mysql

--- pbackmande/pbackmande/settings.py
+++ pbackmande/pbackmande/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'pbackmande.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'servicios',
-    #    'USER': 'root',
-    #    'PASSWORD': '',
-    #    'HOST': '127.0.0.1',
-    #    'PORT':'3306',
-    #},
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "dbmanders",
-        "USER": "iovwkaaltg",
-        "PASSWORD": "852XS077342KV108$",
-        "HOST": "backmanders",
-        "PORT": "",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'servicios',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT':'3306',
+    },
+    #"default": {
+    #    "ENGINE": "django.db.backends.postgresql",
+    #    "NAME": "dbmanders",
+    #    "USER": "iovwkaaltg",
+    #    "PASSWORD": "852XS077342KV108$",
+    #    "HOST": "backmanders",
+    #    "PORT": "",
+    #}
 }
 
 #
